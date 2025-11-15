f5ac8aabd5adbd0b41056e903513c5e43b11155e
Muhammedali <241773@astanait.edu.kz>
2025-11-15 20:13:36 +0500

    some changes and front

--- src/main/java/school/Main.java
+++ src/main/java/school/Main.java
@@ -10,20 +10,27 @@ import school.web.WebServer;
 public class Main {
     public static void main(String[] args) {
         System.setProperty("spring.main.web-application-type", "none");
-        
+
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
         SchoolFacade facade = context.getBean(SchoolFacade.class);
-        
+
+        int port = 8080;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
         try {
-            WebServer webServer = new WebServer(8080);
-            webServer.start();
-            System.out.println("\n=== Web API Server Started ===");
-            System.out.println("Server running at: http://localhost:8080");
+            WebServer server = new WebServer(port);
+            server.start();
+
+            System.out.println("School Management System");
+            System.out.println("Web server running at: http://localhost:" + port);
+
         } catch (Exception e) {
-            System.err.println("Failed to start WebServer: " + e.getMessage());
+            System.err.println("Error starting web server: " + e.getMessage());
             e.printStackTrace();
         }
-        
+
         facade.demonstrateCompleteSystem();
     }
 }
